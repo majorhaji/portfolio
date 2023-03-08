da7fe2297550c53f6160c296cc267fc326b7cc4a
yusuf <majorhaji@github.com>
2023-03-08 14:33:06 +0000

    animation part 1

--- portfolio-cli/src/components/Intro.jsx
+++ portfolio-cli/src/components/Intro.jsx
@@ -1,9 +1,27 @@
 const Intro = () => {
   return (
     <section className="intro">
-      <h1>Hi, I'm Yusuf.</h1>
-      <h3>A software developer by day, superhero by night*</h3>
-      <p></p>
+      <div className="sky">
+        <span className="clouds" style={{ "--i": 11 }}></span>
+
+        <span className="clouds" style={{ "--i": 12 }}></span>
+
+        <span className="clouds" style={{ "--i": 24 }}></span>
+
+        <span className="clouds" style={{ "--i": 10 }}></span>
+
+        <span className="clouds" style={{ "--i": 23 }}></span>
+
+        <span className="clouds" style={{ "--i": 18 }}></span>
+
+        <span className="clouds" style={{ "--i": 16 }}></span>
+
+        <div className="intro-heading">
+          <h1>Hi, I'm Yusuf.</h1>
+          <h3>A software developer by day, superhero by night*</h3>
+          <p></p>
+        </div>
+      </div>
     </section>
   );
 };
