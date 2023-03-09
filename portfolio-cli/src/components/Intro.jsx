3bd2814269bc92c6c613031fbef01f1a09a37a0c
yusuf <majorhaji@github.com>
2023-03-09 10:44:12 +0000

    intro added to middle of clouds

--- portfolio-cli/src/components/Intro.jsx
+++ portfolio-cli/src/components/Intro.jsx
@@ -2,24 +2,24 @@ const Intro = () => {
   return (
     <section className="intro">
       <div className="sky">
-        <span className="clouds" style={{ "--i": 11 }}></span>
+        <span className="clouds" style={{ "--i": 9 }}></span>
 
         <span className="clouds" style={{ "--i": 12 }}></span>
 
-        <span className="clouds" style={{ "--i": 24 }}></span>
+        <span className="clouds" style={{ "--i": 7 }}></span>
 
-        <span className="clouds" style={{ "--i": 10 }}></span>
-
-        <span className="clouds" style={{ "--i": 23 }}></span>
-
-        <span className="clouds" style={{ "--i": 18 }}></span>
-
-        <span className="clouds" style={{ "--i": 16 }}></span>
+        <span className="clouds" style={{ "--i": 8 }}></span>
         <div className="intro-heading">
           <h1>Hi, I'm Yusuf.</h1>
           <h3>A software developer by day, superhero by night*</h3>
           <p></p>
         </div>
+
+        <span className="clouds" style={{ "--i": 13 }}></span>
+
+        <span className="clouds" style={{ "--i": 5 }}></span>
+
+        <span className="clouds" style={{ "--i": 14 }}></span>
       </div>
     </section>
   );
